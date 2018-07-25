4f3a943dc8cdc0b108096fba9b4f850e0a886994
hsekhon <hsekhon@sfu.ca>
2018-07-24 23:13:47 -0700

    working datepicker

--- Players/Controllers/HomeController.cs
+++ Players/Controllers/HomeController.cs
@@ -10,10 +10,10 @@ namespace Players.Controllers
     public class HomeController : Controller
     {
         // GET: Home
-        private PlayersDBEntities _db = new PlayersDBEntities();
+        private PlayersDBEntities db = new PlayersDBEntities();
         public ActionResult Index()
         {
-            return View(_db.Tables.ToList());
+            return View(db.Tables.ToList());
         }
 
        
@@ -33,8 +33,8 @@ namespace Players.Controllers
                 if (!ModelState.IsValid)
                     return View();
 
-                _db.Tables.Add(playerToCreate);
-                _db.SaveChanges();
+                db.Tables.Add(playerToCreate);
+                db.SaveChanges();
 
                 return RedirectToAction("Index");
             }
@@ -44,7 +44,7 @@ namespace Players.Controllers
         // GET: Home/Edit/5
         public ActionResult Edit(int id)
         {
-            var playerToEdit = (from p in _db.Tables
+            var playerToEdit = (from p in db.Tables
                                 where p.Id == id
                                 select p).First();
             return View(playerToEdit);
@@ -56,14 +56,14 @@ namespace Players.Controllers
         {
            
             {
-                var originalPlayer = (from p in _db.Tables
+                var originalPlayer = (from p in db.Tables
                                       where p.Id == playerToEdit.Id
                                       select p).First();
 
                 if (!ModelState.IsValid)
                     return View(originalPlayer);
-                _db.Entry(originalPlayer).CurrentValues.SetValues(playerToEdit);
-                _db.SaveChanges();
+                db.Entry(originalPlayer).CurrentValues.SetValues(playerToEdit);
+                db.SaveChanges();
 
                 return RedirectToAction("Index");
             }
@@ -72,7 +72,7 @@ namespace Players.Controllers
         // GET: Home/Delete/5
         public ActionResult Delete(int id)
         {
-            var playerToDelete = (from p in _db.Tables
+            var playerToDelete = (from p in db.Tables
                                   where p.Id == id
                                   select p).First();
             return View(playerToDelete);
@@ -84,11 +84,11 @@ namespace Players.Controllers
         {
             
             {
-                var playerDelete = (from p in _db.Tables
+                var playerDelete = (from p in db.Tables
                                       where p.Id == id
                                       select p).First();
-                _db.Tables.Remove(playerDelete);
-                _db.SaveChanges();
+                db.Tables.Remove(playerDelete);
+                db.SaveChanges();
 
                 return RedirectToAction("Index");
             }
